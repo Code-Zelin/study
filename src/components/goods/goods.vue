36d8f173888fa8a92a779fa9468a3755b4da7e2c
szl <463515279@qq.com>
2017-02-22 18:20:11 +0800

    修改goods.vue

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    I'm goods!
+  <div class="goods">
+    <div class="goods_menu">
+      <ul>
+        <li v-for="menu in goods">
+          {{menu.name}}
+        </li>
+      </ul>
+    </div>
+
+    <div class="goods_list">
+
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  export default {
+    data () {
+      return {
+        goods: {}
+      }
+    },
+    created () {
+      this.$http.get('/api/goods').then(response => {
+        let vueResponse = response.body
+        if (vueResponse.errno === 0) {
+          this.goods = vueResponse.data
+          console.log(this.goods)
+        }
+      }, response => {
+        // error callback
+      })
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods{
+    width:100%;
+    display: flex;
+    .goods_menu{
+      flex: 0 0 80px;
+      width: 80px;
+    }
+    .goods_list{
+      
+    }
+  }
 </style>
